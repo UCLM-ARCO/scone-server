3c5d4cab5e7d44c2d625bd9e9115ff044d5535a9
David Villa Alises <david.villa@gmail.com>
2021-05-02 22:35:08 +0200

    load scone-knowledge.d dir

--- server.sh
+++ server.sh
@@ -8,6 +8,28 @@ mkdir -p $WORKDIR
 LOG="$WORKDIR/server.log"
 SERVER_PID="$WORKDIR/server.pid"
 
+function scone_slive {
+    kill -0 $server_pid > /dev/null 2>&1
+    return $?
+}
+
+    function stop-server {
+    echo -e "\nstopping scone-server..."
+
+    kill -SIGABRT $server_pid > /dev/null 2>&1
+    sleep 1
+
+    while kill -0 $server_pid > /dev/null 2>&1; do
+	    echo "killing scone-server pid:$server_pid"
+	    kill -SIGKILL $server_pid > /dev/null 2>&1
+        sleep 1
+    done
+
+    rm -f "$SERVER_PID"
+    echo -e "[end] scone-server"
+    exit
+}
+
 # Make sure there isn't already a PID file here.
 if [ -f "$SERVER_PID" ]; then echo "Error: $SERVER_PID file detected.  If you believe the server is still running, find the PID and kill that process.  If the server is not running please delete the file server.pid."; exit; fi
 
@@ -27,26 +49,22 @@ if ! kill -0 $server_pid > /dev/null 2>&1; then
     exit 1
 fi
 
+if [ -d scone-knowledge.d ]; then
+    knowledge_dir=$(realpath scone-knowledge.d)
+    echo "Loading knowledge:"
+    find $knowledge_dir -type f -exec echo - {} \;
+    find $knowledge_dir -type f -exec echo \(load \"{}\"\) \; | ncat localhost 6517 > $LOG 2>&1
+
+    if grep --text "^*****SCONE-ERROR" $LOG; then
+        echo "ERROR loading knowledge, aborting..."
+        stop-server
+    fi
+fi
+
 echo "[ready] scone-server pid:$server_pid port:6517"
+echo "Press C-c to stop"
 
-trap ctrl-c INT QUIT
-
-function ctrl-c {
-    echo -e "\nstopping scone-server..."
-
-    kill -SIGABRT $server_pid > /dev/null 2>&1
-    sleep 1
-
-    while kill -0 $server_pid > /dev/null 2>&1; do
-	    echo "killing scone-server pid:$server_pid"
-	    kill -SIGKILL $server_pid > /dev/null 2>&1
-        sleep 1
-    done
-
-    rm -f "$SERVER_PID"
-    echo -e "[end] scone-server"
-    exit
-}
+trap stop-server INT QUIT
 
 while true; do
     read
